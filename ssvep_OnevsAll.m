f3c1818397210eb9f75f608782fedbc07b4c69e5
Gagandeep <gaganpanwar@gmail.com>
2016-04-19 21:09:50 +0530

    Update ssvep_OnevsAll.m

--- ssvep_OnevsAll.m
+++ ssvep_OnevsAll.m
@@ -169,7 +169,7 @@ for j = 1:10
     confMatKj = confusionmat(kidealDecision, kfinalDecision);
     confMatK = confMatK + confMatKj;
     
-    kb = []; kw = []; kpredict = []; kfinalDecision = [];
+    kb = []; kw = []; kpredict = []; kfinalDecision = []; % to avoid dimension mismatch in the non-equal partition
 end
 confMatK
 perc = bsxfun(@rdivide, confMatK, sum(confMatK,2)) * 100
